b07a41b2527e1eaa33c7c817175a7b02eae04c94
Sawon <141425167+SawonAnis@users.noreply.github.com>
2025-03-05 21:57:23 +0600

    Stack Code

---
+++ Data Structure/STACK/POP.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    Stack<int> numbers = new Stack<int>();
+
+    numbers.Push(1);
+    numbers.Push(2);
+    numbers.Push(3);
+    numbers.Push(4);
+    numbers.Push(5);
+
+   
+      Console.WriteLine("popped numbers: "+numbers.Pop());
+      
+    
+  }
+}
